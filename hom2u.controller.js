67559d9cf9c420a38f5ef411e4fe2955243efe4f
samchong1104 <71488914+samchong1104@users.noreply.github.com>
2023-10-14 17:01:44 +0800

    Added change param

--- hom2u.controller.js
+++ hom2u.controller.js
@@ -1,4 +1,12 @@
 module.exports = async () => {
     console.log("qweqweqwe");
     return "hello";
+
+    function change(param) {
+        if (param > 1) {
+            return 5;
+        } else {
+            return 76;
+        }
+    }
 };
